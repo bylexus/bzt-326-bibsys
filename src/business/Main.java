77661477501c9fe0cd403671f2efb3e0707cf3f6
Alexander Schenkel <aschenkel@kadenpartner.ch>
2017-02-17 16:32:11 +0100

    Vorbereitung für weitere Login-View (z.B. Terminal-Login)

--- src/business/Main.java
+++ src/business/Main.java
@@ -2,15 +2,36 @@ package business;
 
 import frontend.AppLoginView;
 import frontend.ProgramManager;
+import frontend.View;
 
 public class Main {
+	static enum Mode {
+		APP, TERMINAL
+	};
 
 	public static void main(String[] args) {
-		// Start der ersten View (Login):
-		AppLoginView view = new AppLoginView();
-		ProgramManager pm = ProgramManager.getInstance();
+		Mode m = Mode.APP;
+		View view = null;
 		
-		pm.addNext(view);
-		pm.startCurrentView();
+		for (String p : args) {
+			if (p.equals("-terminal")) {
+				m = Mode.TERMINAL;
+			}
+		}
+		
+		switch (m) {
+		case APP:
+			view = new AppLoginView();
+			break;
+		case TERMINAL:
+			// TODO
+		}
+		if (view != null) {
+			// Start der ersten View:
+			ProgramManager pm = ProgramManager.getInstance();
+			pm.addNext(view);
+			pm.startCurrentView();
+
+		}
 	}
 }
